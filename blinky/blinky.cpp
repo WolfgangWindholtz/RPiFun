77cd7ee22cf173ce6d9d95a44c1301494987f8e0
lukeearly <23438839+lukeearly@users.noreply.github.com>
2020-08-12 16:52:56 -0400

    some fun changes
    tabs, k&r parentheses and braces, while 1
    in the words of linus torvalds "Don’t put multiple statements on a single line unless you have something to hide"

--- blinky/blinky.cpp
+++ blinky/blinky.cpp
@@ -1,12 +1,13 @@
 #include <wiringPi.h>
 int main (void)
 {
-  wiringPiSetup () ;
-  pinMode (0, OUTPUT) ;
-  for (;;)
-  {
-    digitalWrite (0, HIGH) ; delay (500) ;
-    digitalWrite (0,  LOW) ; delay (500) ;
-  }
-  return 0 ;
+	wiringPiSetup(); // initializes the wiringPi library
+	pinMode(0, OUTPUT); // sets GPIO pin 0 to OUTPUT mode
+	while (1) { // infinite loop that toggles the light in a 1000 ms period, with a 50% duty cycle
+		digitalWrite(0, HIGH);
+		delay(500);
+		digitalWrite(0, LOW);
+		delay(500);
+	}
+	return 0; // unreachable
 }
