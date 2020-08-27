ccdd1ac596c2834b6d68346d9509726b8c284ae2
Wolfgang <wwindholtz@me.com>
2020-08-27 10:13:17 -0400

    switch to python

--- neopixelPython/tictacttoe.py
+++ neopixelPython/tictacttoe.py
@@ -1,8 +1,65 @@
 class MyClass:
     """A simple example class"""
-    i = 12345
 
-    def f(self):
-        return 'hello world'
+    def __init__(self):
+        self.board = [ [' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
+
+
+    def printArr(self):
+        row =  3
+        for row in range(row):
+             
+                print(self.board[row])
+
+    def placePeice(self, peice, row ,col):
+        self.board[row][col] = peice
+    
+    def clearBoard(self):
+        row,col = 3,3
+        for row in range(row):
+            for col in range(col):
+                placePeice(self,' ',row,col)
+
+    def checkWin(self, p):
+        row,col = 3,3
+        ret = False
+        for row in range(row):
+            isRow = True
+            isColumn = True
+            for col in range(col):
+                if(self.board[row][col] != p):
+                    isRow = False
+
+                
+                if(self.board[row][col] != p):
+                    isColumn = False
+                
+             
+            if(isColumn or isRow):
+                ret = True
+                break
+            if( self.board[row][col] == p and 
+                self.board[row][col] == p and
+                self.board[row][col] == p 
+             ):
+                ret = True; 
+              
+
+            if( self.board[row][col] == p and 
+                self.board[row][col] == p and
+                self.board[row][col] == p
+            ):
+                    ret = True; 
+            
+        return ret    
+            
+               
+                 
+
+    
 x = MyClass()
-print(x.i)         
+x.placePeice('x',1,1)
+x.placePeice('x',1,0)
+x.placePeice('x',1,2)
+print(x.checkWin('x'))
+x.printArr()
