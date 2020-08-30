42cd2faad2320c27443ae7cdfeedfb28fdd7a64b
Wolfgang Windholtz <wwindholtz@me.com>
2020-08-30 10:26:38 -0400

    trying to add gui

--- neopixelPython/gui.py
+++ neopixelPython/gui.py
@@ -1,6 +1,23 @@
-import tkinter
 
-window = tkinter.Tk()
+import tkinter as tk       
 
-while True:
-    print("yeet")
+class Application(tk.Frame):              
+    def __init__(self, master=None):
+        tk.Frame.__init__(self, master)   
+        self.grid()                       
+        self.createWidgets()
+
+    def createWidgets(self):
+        self.quitButton = tk.Button(self, text='Quit',
+            command=self.quit)
+        self.practiceButton = tk.Button(self, text='Quit',
+            command=self.haha)                
+        self.quitButton.grid(column = 0 ,row = 0 )
+        self.practiceButton.grid(column = 1 ,row = 1 )   
+
+    def haha(self):
+        print("yoink")             
+
+app = Application()                       
+app.master.title('Sample application')    
+app.mainloop() 
